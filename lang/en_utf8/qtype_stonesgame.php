6963ab94397649cfa0a88ef9fcbd0eb225c99175
Nazarova Elena <ln15ln@rambler.ru>
2016-05-27 01:11:29 +0400

    Add localization and help page

---
+++ lang/en_utf8/qtype_stonesgame.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The language strings for the stonesgame question type.
+ *
+ * @copyright &copy; 2016 SSAU
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package stonesgame
+ *
+ */
+
+$string['pluginname'] = 'Task C3';
+$string['editingstonesgame'] = 'Edit C3';
+$string['addingstonesgame'] = 'Add C3';
+$string['stonesgame'] = 'Task C3';
+$string['openeditor'] = 'Open window of tree creating (Task 3)';
+$string['failedloadinganswers'] = "Fatal error!";
+
+// HS Editor Javascript strings
+
+$string['jsadd'] = 'Add step';
+$string['jsedit'] = 'Edit step';
+$string['jsdelete'] = 'Delete step';
+$string['jsclearall'] = 'Clear tree';
+$string['jsmark'] = 'Mark win strategy steps';
+$string['jsunmark'] = 'Clear all marks';
+$string['jssavemarks'] = 'Save win strategy';
+$string['jsend'] = 'Mark step as winning';
+$string['jssave'] = 'Save';
+$string['jsok'] = 'Yes';
+$string['jsno'] = 'No';
+$string['jsclosewithout'] = 'Close the window without saving the changes?';
+$string['jswarning'] = 'Warning!';
+$string['jsconfirmdelete'] = 'Are you sure you want to delete this step?';
+
+?>
---
+++ lang/ru_utf8/qtype_stonesgame.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The language strings for the stonesgame question type.
+ *    
+ * @copyright &copy; 2016 SSAU
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package stonesgame
+ *
+ */
+
+$string['pluginname'] = 'Задание C3';
+$string['editingstonesgame'] = 'Редактирование задания C3';
+$string['addingstonesgame'] = 'Добавление задания C3';
+$string['stonesgame'] = 'Задание C3';
+$string['openeditor'] = 'Открыть окно для построения дерева (Задание 3)';
+$string['failedloadinganswers'] = "Возникли проблемы при загрузке ответов..";
+
+// HS Editor Javascript strings
+
+$string['jsadd'] = 'Сделать ход';
+$string['jsedit'] = 'Изменить ход';
+$string['jsdelete'] = 'Удалить ход';
+$string['jsclearall'] = 'Очистить экран';
+$string['jsmark'] = 'Отметить ходы выигрышной стратегии';
+$string['jsunmark'] = 'Снять все метки';
+$string['jssavemarks'] = 'Сохранить выигрышную стратегию';
+$string['jsend'] = 'Отметить ход как заключительный';
+$string['jssave'] = 'Сохранить';
+$string['jsok'] = 'Да';
+$string['jsno'] = 'Нет';
+$string['jsclosewithout'] = 'Закрыть окно без сохранения изменений?';
+$string['jswarning'] = 'Внимание!';
+$string['jsconfirmdelete'] = 'Удалить ход?';
+
+?>
