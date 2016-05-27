1933e637f153b81a65d82fb3fe25db2d28708971
Nazarova Elena <ln15ln@rambler.ru>
2016-05-27 05:02:05 +0400

    Add new folder "Stonesdebug" as a new qtype, which is based on numeric standart qtype and works if delete old row in mdl19_config table,      it installs a new table -  mdl19_stonesdebug, but there is not set installing also "mdl19_stonesdebug_units" in install.xml      (maybe, qtype is standart and so the table was installed in different time with first table) Need to delete mysql.php and postgres7.php

--- edit_stonesgame_form.php
+++ edit_stonesgame_form.php
@@ -23,17 +23,31 @@ class question_edit_stonesgame_form extends question_edit_form {
         global $CFG;
 
         $mform->addElement('header', 'stonesgameheader', get_string('stonesgame', 'qtype_stonesgame'));
+
+        $mform->addElement('text', 'email', get_string('stonesgame')); // Add elements to your form
+        $mform->setType('email', PARAM_NOTAGS);                   //Set type of element
+        $mform->setDefault('email', 'Please enter email');
+        /*$mform->addElement('text', 'penalty', get_string('penaltyfactor', 'quiz'),
+            array('size' => 3));
+        $mform->setType('penalty', PARAM_NUMBER);
+        $mform->addRule('penalty', null, 'required', null, 'client');
+        $mform->setHelpButton('penalty', array('penalty', get_string('penalty', 'quiz'), 'quiz'));
+        $mform->setDefault('penalty', 0.1);*/
         //$mform->addElement
 
         //$mform->addElement('button', 'buttoneditor', get_string('openeditor', 'qtype_stonesgame'),array('onclick'=>'hscheckImages(\''.(get_string('imagealert','qtype_stonesgame')).'\',\''.(get_string('chooseanimage','qtype_stonesgame')).'\',\''.$CFG->wwwroot.'\',this.form)'));
 
-        $mform->addElement('hidden', 'hseditordata');
+        $attributes = ['строка1', 'строка2'];
+        $radioarray=array();
+        $radioarray[] = $mform->createElement('radio', 'yesno', '', get_string('yes'), 1, $attributes);
+        $radioarray[] = $mform->createElement('radio', 'yesno', '', get_string('no'), 0, $attributes);
+        $mform->addGroup($radioarray, 'radioar', '', array(' '), false);
+        $mform->addElement('hidden', 'stone');
     }
 
     function set_data($question) {
-
         if(isset($question->options)){
-            $default_values['hseditordata'] =  stripslashes($question->options->hseditordata);
+            $default_values['stone'] =  stripslashes($question->options->stone);
             $question = (object)((array)$question + $default_values);
         }
         parent::set_data($question);
